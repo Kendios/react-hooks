e60f1473bfa62ccbe86d1c053e9a29dae454bd4b
zhaohaonan <zhaohaonan@weihaizixun.com>
2023-01-04 16:55:30 +0800

    主页布局完成

--- src/views/layout/aside.tsx
+++ src/views/layout/aside.tsx
@@ -8,16 +8,28 @@ function aside() {
   const [router] = useState(router_item)
 
   const navigate = useNavigate()
+  const storageSelectKeys = sessionStorage.getItem('selectKeys')
+  const [selectKeys, setSelectKeys] = useState(storageSelectKeys)
+  const [openKeys, setOpenKeys] = useState(storageSelectKeys)
+
 
   const onClick: MenuProps['onClick'] = e => {
     const keyPath = e.keyPath.reverse().join('/')
     navigate(`/${keyPath}`)
+    setSelectKeys(e.key)
+    sessionStorage.setItem('selectKeys', e.key)
   };
 
   return (
     <>
-      <Menu mode="inline"
-        theme="dark" items={router} onClick={onClick} />
+      <Menu
+        onClick={onClick}
+        mode="inline"
+        theme="dark"
+        defaultOpenKeys={['layout']}
+        defaultSelectedKeys={selectKeys}
+        items={router}
+      />
     </>
   )
 }
